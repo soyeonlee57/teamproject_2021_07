efb494f878d28bbc23c758892265e3fb790b06ed
soyeonlee57 <soyeonlee57@gmail.com>
2021-07-20 14:56:52 +0900

    base_settings

---
+++ doc/common.js
@@ -0,0 +1,107 @@
+gsap.to(".box-1",{
+  x: 100,
+  duration: 1,
+  scrollTrigger:{
+    trigger:'.box-1',
+  }
+  
+});
+
+gsap.to(".box-2",{
+  x: -100,
+  duration: 2,
+  scrollTrigger:{
+    trigger:'.box-2',
+  }
+  
+});
+
+gsap.to(".bike-1",{
+  x: -700,
+  duration: 1.3,
+  scrollTrigger:{
+    trigger:'.bike-1',
+  }
+  
+});
+
+gsap.to(".bike-2",{
+  x: -700,
+  duration: 2,
+  scrollTrigger:{
+    trigger:'.bike-2',
+  }
+  
+});
+
+gsap.to(".page_2_image",{
+  opacity:1,
+  duration: 2.3,
+  scrollTrigger:{
+    trigger:'.page_2_image',
+  }
+  
+});
+
+gsap.to(".research_1",{
+  opacity:1,
+  duration: 2.3,
+  scrollTrigger:{
+    trigger:'.research_1',
+  }
+  
+});
+
+gsap.to(".icon_1",{
+  opacity:1,
+  duration: 2.8,
+  scrollTrigger:{
+    trigger:'.icon_1',
+  }
+  
+});
+
+gsap.to(".target",{
+  opacity:1,
+  duration: 2.8,
+  scrollTrigger:{
+    trigger:'.target',
+  }
+  
+});
+
+gsap.to(".target_1",{
+  opacity:1,
+  duration: 3.2,
+  scrollTrigger:{
+    trigger:'.target_1',
+  }
+  
+});
+
+gsap.to(".target_2",{
+  opacity:1,
+  duration: 3.8,
+  scrollTrigger:{
+    trigger:'.target_2',
+  }
+  
+});
+
+gsap.to(".target_3",{
+  opacity:1,
+  duration: 4,
+  scrollTrigger:{
+    trigger:'.target_3',
+  }
+  
+});
+
+gsap.to(".target_4",{
+  opacity:1,
+  duration: 4.5,
+  scrollTrigger:{
+    trigger:'.target_4',
+  }
+  
+});
